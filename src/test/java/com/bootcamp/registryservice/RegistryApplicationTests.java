e4c3231d95870703a525f63341e83540754d26c3
uCrover <chris66xd@gmail.com>
2022-02-14 12:35:25 -0500

    t1

---
+++ src/test/java/com/bootcamp/registryservice/RegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp.registryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
